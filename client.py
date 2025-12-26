d465f8c2879564ca53936afa4f714f15f3205d17
AbhaySachan0 <abhaysach05@gmail.com>
2025-12-26 13:48:24 +0530

    added client file

---
+++ client.py
@@ -0,0 +1,43 @@
+
+import socket
+import threading
+
+host = "127.0.0.1" 
+port = 44444
+
+nickname = input("Enter your name : ")
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((host,port))
+
+def receive_message():
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+            
+            if(message.startswith("Nickname")):
+                client.send(nickname.encode("ascii"))
+            else:
+                print(message)
+        except:
+            print("error occurred")
+            client.close()
+            break
+
+
+def write_message():
+    while True:
+        try:
+            message = f"{nickname}: {input('')}"
+            client.send(message.encode("ascii"))
+        except:
+            print("server disconnected")
+            client.close()
+            break;
+
+
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write_message)
+write_thread.start()
